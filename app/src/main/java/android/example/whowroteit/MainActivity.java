06ba72f0c422c90d2279800880a6e253a370b4b8
David García Alonso <a20davgaralo@inspedralbes.cat>
2022-03-29 12:32:08 +0200

    Funciona

--- app/src/main/java/android/example/whowroteit/MainActivity.java
+++ app/src/main/java/android/example/whowroteit/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.ref.WeakReference;
 
 public class MainActivity extends AppCompatActivity {
@@ -51,8 +55,52 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-        }
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                JSONArray itemsArray = jsonObject.getJSONArray("items");
 
+                int i = 0;
+                String title = null;
+                String authors = null;
+
+                while (i < itemsArray.length() &&
+                        (authors == null && title == null)) {
+                    // Get the current item information.
+                    JSONObject book = itemsArray.getJSONObject(i);
+                    JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                    // Try to get the author and title from the current item,
+                    // catch if either field is empty and move on.
+                    try {
+                        title = volumeInfo.getString("title");
+                        authors = volumeInfo.getString("authors");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    // Move to the next item.
+                    i++;
+                }
+
+                //OJO! SOLO PILLA EL PRIMER RESULTADO!
+                if (title != null && authors != null) {
+                    mTitleText.get().setText(title);
+                    mAuthorText.get().setText(authors);
+                } else {
+                    mTitleText.get().setText(R.string.no_results);
+                    mAuthorText.get().setText("");
+                }
+
+
+
+            } catch (JSONException e) {
+                // If onPostExecute does not receive a proper JSON string,
+                // update the UI to show failed results.
+                mTitleText.get().setText(R.string.no_results);
+                mAuthorText.get().setText("");
+                e.printStackTrace();
+            }
+        }
 
 
     }
